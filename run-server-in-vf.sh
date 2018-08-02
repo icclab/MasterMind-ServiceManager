ea7c6b4ea26ac2aad64825f2554366fe4b7ec24e
Sean Murphy <sean@gopaddy.ch>
2018-08-02 14:48:23 +0200

    Further resturturing for tidiness...

--- run-server-in-vf.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-python api/app.py --log-level TEST /home/sean/Work/ZHAW/Projects/FINEXT/MasterMind/MasterMind-ServiceManager/api/logging.conf
---
+++ utils/run-server-in-vf.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+python ../api/app.py --log-level TEST /home/sean/Work/ZHAW/Projects/FINEXT/MasterMind/MasterMind-ServiceManager/api/logging.conf
